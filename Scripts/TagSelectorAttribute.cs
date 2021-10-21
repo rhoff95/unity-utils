2755335b4c8ce333a38a5a4901a93eb59fed523c
Robert Hoff <robbyhoff95@gmail.com>
2021-10-20 22:54:11 -0400

    Adds tag scripts

---
+++ Scripts/TagSelectorAttribute.cs
@@ -0,0 +1,9 @@
+﻿﻿using UnityEngine;
+ 
+ namespace rhoff95.UnityUtils
+{
+    public class TagSelectorAttribute : PropertyAttribute
+    {
+        public bool UseDefaultTagFieldDrawer = false;
+    }
+}
