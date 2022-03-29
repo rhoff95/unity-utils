bbfd65a01fb7b0be1a27a268e4838016f0afc4fc
Rob Hoff <robbyhoff95@gmail.com>
2022-03-28 18:39:10 -0600

    Adds audio listener updates

--- Scripts/Audio/PersistentBackgroundAudio.cs
+++ Scripts/Audio/PersistentBackgroundAudio.cs
@@ -1,7 +1,10 @@
+using UnityEngine;
+
 namespace Scripts.Audio
 {
     public class PersistentBackgroundAudio : BackgroundAudio
     {
+        public const string PrefVolume = "VOLUME";
         private static PersistentBackgroundAudio _instance;
 
         protected override void Awake()
@@ -14,8 +17,18 @@ namespace Scripts.Audio
 
             _instance = this;
             DontDestroyOnLoad(transform.gameObject);
+            
+            UpdateVolume();
 
             base.Awake();
+        }
+
+        public static void UpdateVolume()
+        {
+            var audioVolume = PlayerPrefs.GetFloat(PrefVolume, 1f);
+            AudioListener.volume = audioVolume;
+            
+            Debug.Log($"Setting audio listener volume to {audioVolume}");
         }
     }
 }
