4a3fe32ec55d70de28349de392832662b46bb00f
Robert Hoff <robbyhoff95@gmail.com>
2023-07-02 10:52:23 -0400

    Adds follow camera script

---
+++ Scripts/Camera/FollowCamera.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace Plugins.unity_utils.Scripts.Camera
+{
+    public class FollowCamera : MonoBehaviour
+    {
+        public GameObject targetGameObject;
+        [Range(0f, 5f)] public float smoothTime = 1f;
+        public float targetZ = -10f;
+
+        private Vector3 _currentVelocity;
+
+        private void Update()
+        {
+            var nextPosition = Vector3.SmoothDamp(transform.position,
+                targetGameObject.transform.position, ref _currentVelocity, smoothTime);
+            transform.position = new Vector3(
+                nextPosition.x, nextPosition.y, targetZ);
+        }
+    }
+}
