c6659d94dcc06d6293018b4d78bcc01caa7930f1
Robert Hoff <robbyhoff95@gmail.com>
2023-07-02 11:40:00 -0400

    Adds shadow caster scripts

---
+++ Editor/ShadowCasterGeneratorEditor.cs
@@ -0,0 +1,37 @@
+﻿using Plugins.unity_utils.Scripts.Lighting;
+using UnityEditor;
+using UnityEngine;
+
+namespace Plugins.unity_utils.Editor
+{
+    [CustomEditor(typeof(ShadowCaster2DTileMap))]
+    public class ShadowCastersGeneratorEditor : UnityEditor.Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            DrawDefaultInspector();
+            ShadowCaster2DTileMap generator = (ShadowCaster2DTileMap)target;
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+            EditorGUILayout.Space();
+
+
+            if (GUILayout.Button("Generate"))
+            {
+
+                generator.Generate();
+
+            }
+
+            EditorGUILayout.Space();
+            if (GUILayout.Button("Destroy All Children"))
+            {
+
+                generator.DestroyAllChildren();
+
+            }
+        }
+
+    }
+}
